32c5fa6c93a6ef4dc8fd7f3fb2194f1e266844c9
jaejunlee11 <sktks11@naver.com>
2023-05-22 01:06:01 +0900

    EmploymentRegister 기능구현

--- EmploymentInformation.h
+++ EmploymentInformation.h
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
+#define MAX_STRING 32
+using namespace std;
 class EmploymentInformation{
-    public:
-
     private:
+        string work;
+        int peopleNumber;
+        string deadline;
+        string name;
+        bool isClosed;
+        int businessNumber;
+    public:
+        EmploymentInformation(string work,int peopleNumber,string deadline,string name,bool isClosed,int businessNumber);
+        
 };
