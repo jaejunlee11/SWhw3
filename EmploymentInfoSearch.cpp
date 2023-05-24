4b2b9bdf326b14ddb3350f6589484c394f65d19d
wltjddn98@naver.com <wltjddn98@naver.com>
2023-05-24 13:48:02 +0900

    변경

--- EmploymentInfoSearch.cpp
+++
@@ -1,9 +0,0 @@
-#include "EmploymentInfoSearch.h"
-#include "EmploymentInfoSearchUI.h"
-#include "EmploymentInformation.h"
-
-void EmploymentInfoSearch::findEmploymentInformation(string companyName, int businessNumber, string work, int peopleNumber, string deadline)
-{
-
-
-}
