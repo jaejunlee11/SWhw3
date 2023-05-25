40a5e7ded0c8ac2b15877e25483a1c3f3a33260c
jaejunlee11 <sktks11@naver.com>
2023-05-26 02:55:39 +0900

    등록된 채용 정보 조회 구현 완료

--- EmploymentInfoSearch.cpp
+++ EmploymentInfoSearch.cpp
@@ -3,24 +3,46 @@
 using namespace std;
 
 void EmploymentInfoSearch::findEmploymentInformation(string companyName) {
-	format infoArr[0] = employInfo.getEmployee();
+	Employee findArr[50];
+	int findCount=0;
+    businessUser = businessUserCollection->findFirst();
+	int count=businessUserCollection->getSize();
+	employInfoCollection=businessUser->listEmploymentInformation();
+	employInfo = employInfoCollection.findFirst();
+	Employee infoArr[50];
+	infoArr[0] = employInfo->getEmploymentInformation();
 	int count = employInfoCollection.getSize();
-
 	if (infoArr[0].companyName == companyName) {
-		employInfo->getEmploymentInformation();
-		return;
+		findArr[findCount]=infoArr[0];
+		findCount+=1;s
 	}
-	
-	for (int i = 1; i < count; i++) {
-		employInfo = employmentInformationCollection.getNext();
-		infoArr[i] = employInfo.getEmployee();
-		if (infoArr[i].companyName == companyName) {
-			employInfo->getEmploymentInformation();
-			return;
+	for (int j = 1; j < count; j++) {
+		employInfo = employInfoCollection.getNext();
+		infoArr[j] = employInfo->getEmploymentInformation();
+		if (infoArr[j].companyName == companyName) {
+			findArr[findCount]=infoArr[j];
+			findCount+=1;
+		}
+	}
+	for (int i=1; i< count; i++){
+		employInfo = businessUserCollection->getNext();
+		Employee infoArr[50];
+		infoArr[0] = employInfo->getEmploymentInformation();
+		int count = employInfoCollection.getSize();
+		if (infoArr[0].companyName == companyName) {
+			findArr[findCount]=infoArr[0];
+			findCount+=1;
+		}
+		for (int j = 1; j < count; j++) {
+			employInfo = employInfoCollection.getNext();
+			infoArr[j] = employInfo->getEmploymentInformation();
+			if (infoArr[j].companyName == companyName) {
+				findArr[findCount]=infoArr[j];
+				findCount+=1;
+			}
 		}
 	}
 }
-
 
 void EmploymentInfoSearch::run(BusinessUser* businessUser) {
 	this->businessUser = businessUser;
--- EmploymentInfoSearch.h
+++ EmploymentInfoSearch.h
@@ -4,15 +4,18 @@
 #include "EmploymentInfoSearchUI.h"
 #include "EmploymentInformation.h"
 #include "BusinessUser.h"
+#include "BusinessUserCollection.h"
+#include "EmploymentInformationCollection.h"
 using namespace std;
 
 class EmploymentInfoSearch {
 private:
 	BusinessUser* businessUser;
-	EmploymentInformation employmentInformation;
+	BusinessUserCollection* businessUserCollection;
+	EmploymentInformation* employInfo;
 	EmploymentInfoSearchUI employmentInfoSearchUI;
 	EmploymentInformationCollection employInfoCollection;
 public:
 	void findEmploymentInformation(string companyName);
-	void run(BusinessUser* businessUser);
+	void run(BusinessUserCollection* businessUserCollection);
 };
