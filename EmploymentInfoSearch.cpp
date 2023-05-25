2a5b7808bf5ceaab59f510a2719e437efd090186
jaejunlee11 <sktks11@naver.com>
2023-05-26 03:33:26 +0900

    채용 정보 검색 구현 완료

--- EmploymentInfoSearch.cpp
+++ EmploymentInfoSearch.cpp
@@ -1,10 +1,15 @@
 #include "EmploymentInfoSearch.h"
 #include <string>
 using namespace std;
-
-void EmploymentInfoSearch::findEmploymentInformation(string companyName) {
+/*
+	함수 이름 : EmploymentInfoSearch::findEmploymentInformation()
+	기능	  : 검색한 회사이름이 일치하는 채용 정보 리스트 반환
+	전달 인자 : companyName->회사 이름
+	반환값    : findArr -> 채용 정보 리스트
+*/
+Employee* EmploymentInfoSearch::findEmploymentInformation(string companyName) {
 	Employee findArr[50];
-	int findCount=0;
+	findCount=0;
     businessUser = businessUserCollection->findFirst();
 	int count=businessUserCollection->getSize();
 	employInfoCollection=businessUser->listEmploymentInformation();
@@ -14,7 +19,7 @@ void EmploymentInfoSearch::findEmploymentInformation(string companyName) {
 	int count = employInfoCollection.getSize();
 	if (infoArr[0].companyName == companyName) {
 		findArr[findCount]=infoArr[0];
-		findCount+=1;s
+		findCount+=1;
 	}
 	for (int j = 1; j < count; j++) {
 		employInfo = employInfoCollection.getNext();
@@ -25,7 +30,10 @@ void EmploymentInfoSearch::findEmploymentInformation(string companyName) {
 		}
 	}
 	for (int i=1; i< count; i++){
-		employInfo = businessUserCollection->getNext();
+		businessUser = businessUserCollection->getNext();
+		int count=businessUserCollection->getSize();
+		employInfoCollection=businessUser->listEmploymentInformation();
+		employInfo = employInfoCollection.findFirst();
 		Employee infoArr[50];
 		infoArr[0] = employInfo->getEmploymentInformation();
 		int count = employInfoCollection.getSize();
@@ -42,11 +50,25 @@ void EmploymentInfoSearch::findEmploymentInformation(string companyName) {
 			}
 		}
 	}
+	return findArr;
 }
-
-void EmploymentInfoSearch::run(BusinessUser* businessUser) {
-	this->businessUser = businessUser;
-	employInfoCollection = businessUser->listEmploymentInformation();
+/*
+	함수 이름 : EmploymentInfoSearch::findCountSize()
+	기능	  : 검색한 회사이름이 일치하는 채용 정보 리스트 크기 반환
+	전달 인자 : 없음
+	반환값    : findCount->채용 정보 리스트 크기
+*/
+int EmploymentInfoSearch::findCountSize(){
+	return findCount;
+};
+/*
+	함수 이름 : EmploymentInfoSearch::run()
+	기능	  : control class 생성 후 채용 정보 검색 시작
+	전달 인자 : businessUserCollection->business user 정보가 모두 담긴 collection
+	반환값    : 없음
+*/
+void EmploymentInfoSearch::run(BusinessUserCollection* businessUserCollection) {
+	this->businessUserCollection = businessUserCollection;
 	employmentInfoSearchUI = EmploymentInfoSearchUI();
 	employmentInfoSearchUI.inputEmploymentInformation(this);
 }
--- EmploymentInfoSearchUI.cpp
+++ EmploymentInfoSearchUI.cpp
@@ -3,14 +3,22 @@
 #define INPUT_FILE_NAME "input.txt"
 #define OUTPUT_FILE_NAME "output.txt"
 using namespace std;
-
+/*
+	함수 이름 : EmploymentInfoSearchUI::inputEmploymentInformation()
+	기능	  : 검색한 회사이름이 일치하는 채용 정보 출력
+	전달 인자 : control->control class pointer
+	반환값    : 없음
+*/
 void EmploymentInfoSearchUI::inputEmploymentInformation(EmploymentInfoSearch* control)
 {
 	FILE* in_fp = fopen(INPUT_FILE_NAME, "r+");
 	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
-
+	Employee* infoArr;
 	fscanf(in_fp, "%s", companyName);
-	control->findEmploymentInformation(companyName);
+	infoArr=control->findEmploymentInformation(companyName);
+	int count=control->findCountSize();
 	fprintf(out_fp, "4.1. 채용 정보 검색\n");
-	fprintf(out_fp, "%s %d %s %d %s", infoArr[i].companyName, infoArr[i].businessNumber, infoArr[i].work, infoArr[i].peopleNumber, infoArr[i].deadline);
+	for(int i=0;i<count;i++){
+		fprintf(out_fp, "%s %d %s %d %s", infoArr[i].companyName, infoArr[i].businessNumber, infoArr[i].work, infoArr[i].peopleNumber, infoArr[i].deadline);
+	}
 };
